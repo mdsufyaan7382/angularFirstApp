d204c5308a6ca4ea4cd98d9e0d1ad227379d965e
sufyaan khan <sufyaan7382@gmail.com>
2023-08-08 11:31:06 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  userID: string='';
+  password: string='';
+
+  login() {
+    // Replace this with actual authentication logic
+    if (this.userID === 'yourUsername' && this.password === 'yourPassword') {
+      console.log('Login successful');
+    } else {
+      console.log('Login failed');
+    }
+  }
+
+}
